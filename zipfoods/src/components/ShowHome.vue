d1355efc0b47cd370a6857cc4b82acf4273ebd57
Claudio Bustamante <claudiomanuelboston@gmail.com>
2019-11-07 07:16:20 -0500

    pushing code after changes to repo

---
+++ zipfoods/src/components/ShowHome.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <p>Claudio's ZipFoods.</p>
+
+        <show-featured :category='featuredCategory'></show-featured>
+    </div>
+</template>
+
+<script>
+import ShowFeatured from './ShowFeatured.vue';
+
+export default {
+    name: 'ShowHome',
+    components: { ShowFeatured },
+    data: function() {
+        return {
+            featuredCategory: 'snacks'
+        };
+    }
+};
+</script>
